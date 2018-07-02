b759aa424c12a830e2dbe3f955e4fe4568c771bc
kelvintankaiboon <kelvintan.official@gmail.com>
2018-07-02 09:49:43 +0800

    commit2/7,0949
    .env file implemented

--- orbital/settings.py
+++ orbital/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'rd^4$h!xm(eq)ha1_%69ii2p@y^y%3on0(k%n7-+8*6!(===&e'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -91,10 +92,10 @@ WSGI_APPLICATION = 'orbital.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'orbital',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST':'localhost',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST':config('DB_HOST'),
         'PORT': ''
     }
 }
@@ -138,9 +139,9 @@ DATETIME_FORMAT = "m/d/Y, H:i"
 
 # STATIC_URL = '/static/'
 # for aws s3 trial by Kelvin
-AWS_ACCESS_KEY_ID = 
-AWS_SECRET_ACCESS_KEY = 
-AWS_STORAGE_BUCKET_NAME = 
+AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 AWS_LOCATION = 'static'
 
