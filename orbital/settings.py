286b38a1d795a3bedee8fb4e0cb21215248b8165
kelvintankaiboon <kelvintan.official@gmail.com>
2018-07-17 15:49:41 +0800

    commit17/7,1549
    corrected syntax errors in settings.py

--- orbital/settings.py
+++ orbital/settings.py
@@ -93,9 +93,9 @@ WSGI_APPLICATION = 'orbital.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'heroku_627a15130df64af'
-        'USER': 'b34044567f4184'
-        'PASSWORD': '5bf87ac6'
+        'NAME': 'heroku_627a15130df64af',
+        'USER': 'b34044567f4184',
+        'PASSWORD': '5bf87ac6',
         'HOST':'us-cdbr-iron-east-04.cleardb.net'
     }
 }
